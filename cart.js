4fdc22c3f33bbd390f4979d5f28a7c250f77cf9c
ArhaanmTiwari <166734692+ArhaanmTiwari@users.noreply.github.com>
2026-03-29 11:13:40 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -14,6 +14,17 @@ window.addToCart = function(productName, price) {
   alert(productName + " added to cart!");
   updateCartBubble();
 }
+window.BuyNow = function(productName, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({
+    name: productName,
+    price: price,
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  window.location.href='checkout.html
+}
 
 // Load cart items into sidebar
 window.loadCart = function() {
